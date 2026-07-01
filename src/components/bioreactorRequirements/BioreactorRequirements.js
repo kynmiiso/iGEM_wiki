ab178ec9c5fdc4f62ebb84590b43047326d5654a
Sara Parvaresh Rizi <sara_parvaresh@yahoo.com>
2026-07-01 13:54:25 -0400

    fixed sizing issues

--- src/components/bioreactorRequirements/BioreactorRequirements.js
+++ src/components/bioreactorRequirements/BioreactorRequirements.js
@@ -213,6 +213,13 @@ export function BioreactorRequirements() {
           )}
           <TableScroll>
             <Table>
+              <colgroup>
+                <col style={{ width: "16%" }} />
+                <col style={{ width: "25%" }} />
+                <col style={{ width: "20%" }} />
+                <col style={{ width: "20%" }} />
+                <col style={{ width: "19%" }} />
+              </colgroup>
               <thead>
                 <tr>
                   {REQUIREMENT_COLUMNS.map((col) => (
@@ -528,7 +535,15 @@ const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
   font-size: 0.85rem;
-  min-width: 720px;
+  table-layout: fixed;
+
+  /* Override the global MDX prose table cap (max-width: 48rem) from the
+     article template so the table fills its container. Tripled selector
+     (&&&) outranks the template's ".article table" specificity. */
+  &&& {
+    width: 100%;
+    max-width: none;
+  }
 
   th,
   td {
@@ -537,6 +552,8 @@ const Table = styled.table`
     border-right: 1px solid var(--color-border);
     text-align: left;
     vertical-align: top;
+    word-break: break-word;
+    overflow-wrap: break-word;
   }
 
   th:last-child,
@@ -554,10 +571,6 @@ const Table = styled.table`
     text-transform: uppercase;
     white-space: nowrap;
     z-index: 1;
-  }
-
-  thead th:first-child {
-    min-width: 12rem;
   }
 
   tbody tr:last-child td {
@@ -662,12 +675,12 @@ const CellImages = styled.div`
 
   a {
     display: block;
-    max-width: min(280px, 100%);
+    max-width: 100%;
   }
 
   img {
     width: 100%;
-    max-width: min(280px, 100%);
+    max-width: 100%;
     height: auto;
     max-height: 220px;
     object-fit: contain;
